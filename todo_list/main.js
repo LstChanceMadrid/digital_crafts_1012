0ff793578ee6c0f9bea7f9cba5199abc813ddcde
LstChanceMadrid <michael.madrid@live.com>
2018-10-13 11:40:41 -0500

    preventing legend from sorting

--- todo_list/main.js
+++ todo_list/main.js
@@ -5,24 +5,23 @@ let taskAddButton = $('#task-add-button');
 
 let pendingTasks = $('#pending-tasks');
 
-let pendingTasksLegend = $('#pending-tasks-legend')
+let pendingTasksLegend = $('#pending-tasks-legend');
 
-let completedTasks = $('#completed-tasks')
+let completedTasks = $('#completed-tasks');
 
-let completedTasksLegend = $('#completed-tasks-legend')
-
-let taskList = $('.task-list')
+let completedTasksLegend = $('#completed-tasks-legend');
 
 // the button that creates a task
 taskAddButton.click(function() {
 
-    let taskCheckbox = $('<input class="task-checkbox" type="checkbox" />')
+    let taskCheckbox = $('<input class="task-checkbox" type="checkbox" />');
 
-    let taskTitle = $('<h2 class="task-title">')
+    let taskTitle = $('<h2 class="task-title">');
+
     let taskName = taskInput.val();
-    taskTitle.html(taskName)
+    taskTitle.html(taskName);
 
-    let taskRemoveButton = $('<input id="task-remove-button" class="task-remove-button" type="button" value="Remove" />')
+    let taskRemoveButton = $('<input id="task-remove-button" class="task-remove-button" type="button" value="Remove" />');
 
     // creates the task with a checkbox, title, and remove button
     let taskContainer = $('<div class="task-container">');
@@ -30,43 +29,63 @@ taskAddButton.click(function() {
     taskContainer.append(taskName);
     taskContainer.append(taskRemoveButton);
 
+
     // automatically adds the new task to the pending tasks list
-    pendingTasks.append(taskContainer)
+    pendingTasks.append(taskContainer);
 
     // moves the task from one list to the other
     taskCheckbox.click(function() {
         if (taskCheckbox.prop('checked')) {
-            taskContainer.appendTo(completedTasks)
+            taskContainer.appendTo(completedTasks);
         } else {
-            taskContainer.appendTo(pendingTasks)
+            taskContainer.appendTo(pendingTasks);
         }
-    })
+    });
 
     // removes the task from the current parrent list
     taskRemoveButton.click(function () {
-        taskContainer.detach()
-    })
+        taskContainer.detach();
+    });
 
+
+
+    // allows for the tasks to be sorted by drag and drop
+    let taskList = $('.task-list');
     taskList.sortable({
-        appendTo: document.body
-    })
+        appendTo: document.body,
+        cancel: '.task-list-legend'
+    });
 
-    let appendTo = taskList.sortable('option', 'appendTo')
-    taskList.sortable('option', 'appendTo', document.body)
+    let appendTo = taskList.sortable('option', 'appendTo');
+    taskList.sortable('option', 'appendTo', document.body);
 
+    // allows for the pending tasks to be drag and dropped to the completed tasks
     pendingTasks.sortable({
-        connectWith: completedTasks
-    })
+        connectWith: completedTasks,
+        cancel: '.task-list-legend'
+    });
 
-    let connectWithCompleted = pendingTasks.sortable('option', 'connectWith')
-    taskList.sortable('option', 'connectWithCompleted', completedTasks)
+    let connectWithCompleted = pendingTasks.sortable('option', 'connectWith');
+    taskList.sortable('option', 'connectWithCompleted', completedTasks);
 
+    // allows for the completed tasks to be drag and dropped to the pending tasks
     completedTasks.sortable({
-        connectWith: pendingTasks
-    })
+        connectWith: pendingTasks,
+        cancel: '.task-list-legend'
+    });
 
-    let connectWithPending = completedTasks.sortable('option', 'connectWith')
-    taskList.sortable('option', 'connectWithPending', pendingTasks)
-  
-})
+    let connectWithPending = completedTasks.sortable('option', 'connectWith');
+    taskList.sortable('option', 'connectWithPending', pendingTasks);
+
+// if ondrop = blah
+
+    // console.log()
+    // console.log()
+
+    // if (completedTasksLegend.parent().is(taskContainer.parent()) === true) {
+    //     console.log('yay');
+    // if (pendingTasksLegend.parent().is(taskContainer.parent()) ){
+    //     console.log('no')
+    // }
+});
 
